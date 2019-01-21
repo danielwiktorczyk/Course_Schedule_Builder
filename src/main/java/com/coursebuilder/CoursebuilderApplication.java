439b132224b1b23c7b6367e941a9be7ada1142a7
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2019-01-21 15:46:23 -0500

    Pooping

--- src/main/java/com/coursebuilder/CoursebuilderApplication.java
+++ src/main/java/com/coursebuilder/CoursebuilderApplication.java
@@ -8,6 +8,7 @@ public class CoursebuilderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursebuilderApplication.class, args);
+        System.out.println("poop");
     }
 
 }
