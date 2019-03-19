544bcdb395c6a72f3217cab9c02e2d295d290166
GwenLegate <gwendolyne_l@outlook.com>
2019-03-19 09:41:15 -0400

    created functionality for a user to specify time preferences for the schedule builder Added attribute userPrefs to Class User to store the user preferences and methods to access and modifty the userPrefs data

--- src/main/java/com/CourseScheduleBuilder/controller/UserPreferencesController.java
+++ src/main/java/com/CourseScheduleBuilder/controller/UserPreferencesController.java
@@ -1,5 +1,6 @@
 package com.CourseScheduleBuilder.controller;
 
+import com.CourseScheduleBuilder.Model.UserPreferences;
 import com.CourseScheduleBuilder.Services.UserPreferencesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,8 +17,54 @@ public class UserPreferencesController {
     @PostMapping("/userPreferences")
     @CrossOrigin
     @ResponseBody
-    public boolean requestUserPreferences(/* //TODO */) {
-        return true;
+    public boolean requestUserPreferences(String email, String day, Integer prefStartTime, Integer prefEndTime) {
+        UserPreferences newPreference;
+
+        if(day.equalsIgnoreCase("monday")){
+            newPreference.setMonday(true);
+            newPreference.setTuesday(false);
+            newPreference.setWednesday(false);
+            newPreference.setThursday(false);
+            newPreference.setFriday(false);
+        }
+        else if(day.equalsIgnoreCase("tuesday")){
+            newPreference.setTuesday(true);
+            newPreference.setMonday(false);
+            newPreference.setWednesday(false);
+            newPreference.setThursday(false);
+            newPreference.setFriday(false);
+        }
+        else if(day.equalsIgnoreCase("wednesday")){
+            newPreference.setWednesday(true);
+            newPreference.setTuesday(false);
+            newPreference.setMonday(false);
+            newPreference.setThursday(false);
+            newPreference.setFriday(false);
+        }
+        else if(day.equalsIgnoreCase("thursday")){
+            newPreference.setThursday(true);
+            newPreference.setTuesday(false);
+            newPreference.setWednesday(false);
+            newPreference.setMonday(false);
+            newPreference.setFriday(false);
+        }
+        else if(day.equalsIgnoreCase("friday")){
+            newPreference.setMonday(true);
+            newPreference.setTuesday(false);
+            newPreference.setWednesday(false);
+            newPreference.setThursday(false);
+            newPreference.setMonday(false);
+        }
+        else{
+            System.out.println("A day for this preference option was not specified, preferences not updated");
+            return false;
+        }
+
+        newPreference.setStartTime(prefStartTime);
+        newPreference.setEndTime(prefEndTime);
+
+        modifyUserPrefs(email, newPreference);
+
         // using userPreferences for logic
     }
 }
